79b9a6dfeef068db87265c1f99ee5400356a7238
Shabbir <shabbir.reshamwala@trigyn.com>
2017-02-01 12:34:46 +0530

    Changes suggested by Pareview.sh

--- src/Controller/VCardController.php
+++ src/Controller/VCardController.php
@@ -6,23 +6,25 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Session\AccountInterface;
 
 class VCardController extends ControllerBase {
+  
   /**
-   * VCard for direct download
+   * VCard for direct download.
    *
-   * Prints to the browser for direct download, then exits
+   * Prints to the browser for direct download, then exits.
    */
   public function vcardFetch(AccountInterface $user) {
     $vcard = vcard_get($user);
-    $vcard_txt = $vcard->fetch();
+    $vcard_text = $vcard->fetch();
 
     if (!empty($vcard_text)) {
       header('Content-type: text/x-vcard; charset=UTF-8');
       header('Content-Disposition: attachment; filename="' . uniqid() . '.vcf"');
-      print $vcard_txt;
+      print $vcard_te	xt;
       exit;
     }
     else {
       return t("Error building vcard");
     }
   }
+  
 }
